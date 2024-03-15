7709404bec3035a824d264c766d434c55d8244df
山遥路远 <2214981880@qq.com>
2024-03-15 10:50:37 +0800

    feat: 轮播图 + 分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
-//
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import CustomNarBar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
+import XtxSwiper from '../../components/XtxSwiper.vue'
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+// 页面加载
+onLoad(async () => {
+  await Promise.all([getHomeBannerData(), getHomeCategoryData()])
+})
 </script>
 
 <template>
+  <CustomNarBar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel :list="categoryList" />
   <view class="index">index</view>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
 </template>
 
 <style lang="scss">
